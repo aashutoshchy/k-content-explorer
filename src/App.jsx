c63c5a263aa8cfac705363c38a6a12e61f05ce32
Aashutosh Chaudhary <newashutosh.team7@gmail.com>
2026-08-28 13:47:34 +0545

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <>
+      K-Content Explorer
+      <img src={logo} className="logo" alt="logo" />
+    </>
+  )
+}
+
+export default App
